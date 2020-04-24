8d65e3d680c7d8bb363a1005b6f03b4977721280
Dodi <201808180@post.au.dk>
2020-04-24 11:42:50 +0200

    Implemented Integration test step 1

---
+++ Microwave.Integrationtest/IT1_LightOutput.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+
+namespace Microwave.Integrationtest
+{
+    [TestFixture]
+    public class IT1_LightOutput
+    {
+        private Light _uut;
+        private IOutput _output;
+        private StringWriter _stw;
+
+        [SetUp]
+        public void Setup()
+        {
+            _output = new Output();
+            _uut = new Light(_output);
+            _stw = new StringWriter();
+            Console.SetOut(_stw);
+        }
+
+        [Test]
+        public void TurnOn_WasOff_CorrectOutput()
+        {
+            _uut.TurnOn();
+            Assert.That(_stw.ToString(), Contains.Substring("on"));
+        }
+
+        [Test]
+        public void TurnOff_WasOn_CorrectOutput()
+        {
+            _uut.TurnOn();
+            _uut.TurnOff();
+
+            Assert.That(_stw.ToString(), Contains.Substring("off"));
+        }
+
+        [Test]
+        public void TurnOn_WasOn_CorrectOutput()
+        {
+            _uut.TurnOn();
+            _uut.TurnOn();
+            Assert.That(_stw.ToString(), Contains.Substring("on"));
+        }
+
+        [Test]
+        public void TurnOff_WasOff_CorrectOutput()
+        {
+            _uut.TurnOff();
+            Assert.That(_stw.ToString(), Is.Empty);
+        }
+    }
+}
---
+++ Microwave.Integrationtest/IT4_CookControllerDisplayPowertubeTimer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NUnit.Framework;
+
+namespace Microwave.Integrationtest
+{
+    [TestFixture]
+    public class IT4_CookControllerDisplayPowertubeTimer
+    {
+        private CookController _uut;
+        
+        private IDisplay _display;
+        private IPowerTube _pt;
+        private ITimer _timer;
+        private IOutput _output;
+
+        [SetUp]
+        public void Setup()
+        {
+            _output = new Output();
+            _display = new Display(_output);
+            _pt = new PowerTube(_output);
+            _timer = new Timer();
+
+            _uut = new CookController(_timer,  _display, _pt);
+        }
+
+        //[Test]
+        //public void StartCooking_
+
+
+
+
+
+    }
+}
